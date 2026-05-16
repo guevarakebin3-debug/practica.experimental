82af56897cb46bdd2fa791ef2c02722b9c983e83
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:29:24 -0500

    Update actualizar_lista.py

--- Bloque_12/actualizar_lista.py
+++ Bloque_12/actualizar_lista.py
@@ -1,1 +1,20 @@
+def ejecutar():
 
+
+    print("Ejercicio adicional")
+    print("Actualizar lista con manejo de IndexError")
+
+    lista = [10, 20, 30]
+
+    print("Lista:", lista)
+
+    try:
+        pos = int(input("Ingrese la posición (0-2): "))
+        nuevo_valor = int(input("Ingrese el nuevo valor: "))
+
+        lista[pos] = nuevo_valor
+
+        print("Lista actualizada:", lista)
+
+    except IndexError:
+        print("Error: esa posición no existe en la lista")
