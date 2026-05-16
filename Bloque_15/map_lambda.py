b0436e29015749806932a2812d01adbbefd70d50
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:50:49 -0500

    Add function to increment list elements using map

--- Bloque_15/map_lambda.py
+++ Bloque_15/map_lambda.py
@@ -1,1 +1,16 @@
+def ejecutar():
 
+    print("Bloque 15 - map + lambda")
+
+    print("Ejercicio 1 - Incrementa en 1 cada elemento de la lista")
+
+    numeros = []
+
+    for i in range(3):
+        num = int(input(f"Ingrese número {i + 1}: "))
+        numeros.append(num)
+
+    resultado = list(map(lambda x: x + 1, numeros))
+
+    print("Lista original:", numeros)
+    print("Resultado:", resultado)
