f0db1b22fbae981f0fcf1e295c8fa1fe3780c29f
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:25:30 -0500

    Update duplicados.py

--- Bloque_11/duplicados.py
+++ Bloque_11/duplicados.py
@@ -1,1 +1,20 @@
+def ejecutar():
 
+    print("Bloque 11")
+
+    print("Ejercicio 2")
+    print("Eliminar duplicados con set")
+
+    cantidad = int(input("¿Cuántos números vas a ingresar?: "))
+
+    lista = []
+
+    for i in range(cantidad):
+        num = int(input(f"Ingrese el número {i + 1}: "))
+        lista.append(num)
+
+    print("\nLista original:", lista)
+
+    sin_duplicados = list(set(lista))
+
+    print("Sin duplicados:", sin_duplicados)
