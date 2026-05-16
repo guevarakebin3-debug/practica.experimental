b4596d6ebce3c1ffb5a2c607a75ca9ffc94975a1
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:19:14 -0500

    Update tipos_datos.py

--- Bloque_2/tipos_datos.py
+++ Bloque_2/tipos_datos.py
@@ -1,1 +1,28 @@
+def ejecutar():
 
+    print("Bloque 2")
+    print("Ejercicio 1")
+
+    print("DECLARACIÓN DE VARIABLES SIMPLES Y COMPUESTAS")
+
+    entero = 19
+    flotante = 3.14
+    cadena = "Hola Python"
+    booleano = True
+    nulo = None
+
+    lista = [1, 2, 3, "python"]
+    tupla = (10, 20, 30)
+    diccionario = {"nombre": "Juan", "edad": 25}
+    conjunto = {1, 2, 3, 4}
+
+    print("Entero:", entero)
+    print("Float:", flotante)
+    print("String:", cadena)
+    print("Booleano:", booleano)
+    print("None:", nulo)
+
+    print("Lista:", lista)
+    print("Tupla:", tupla)
+    print("Diccionario:", diccionario)
+    print("Conjunto:", conjunto)
