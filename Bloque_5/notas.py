21ddbbee449bad298fa1827863412f9fb3165a6b
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:44:28 -0500

    Update notas.py

--- Bloque_5/notas.py
+++ Bloque_5/notas.py
@@ -1,1 +1,15 @@
+def ejecutar():
 
+    print("Ejercicio 2")
+    print("Nivel de rendimiento académico")
+
+    nota = int(input("Ingrese su nota: "))
+
+    if nota >= 90:
+        print("A")
+    elif nota >= 80:
+        print("B")
+    elif nota >= 70:
+        print("C")
+    else:
+        print("D")
