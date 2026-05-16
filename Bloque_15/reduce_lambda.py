96ef9a633ea9405ed3b254f80379794df0f9737e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:51:22 -0500

    Update reduce_lambda.py

--- Bloque_15/reduce_lambda.py
+++ Bloque_15/reduce_lambda.py
@@ -1,1 +1,33 @@
+from functools import reduce
 
+def ejecutar():
+
+    print("Bloque 15 - reduce + lambda")
+
+    print("Ejercicio 3 - Multiplica todos los elementos")
+
+    numeros = []
+
+    for i in range(3):
+        num = int(input(f"Ingrese número {i + 1}: "))
+        numeros.append(num)
+
+    resultado = reduce(lambda x, y: x * y, numeros)
+
+    print("\nLista:", numeros)
+    print("Resultado de la multiplicación:", resultado)
+
+
+    print("\nEjercicio adicional")
+
+    numeros = []
+
+    for i in range(3):
+        num = int(input(f"Ingrese número {i + 1}: "))
+        numeros.append(num)
+
+    print("\nLista ingresada:", numeros)
+
+    resultado = reduce(lambda x, y: x * y, numeros)
+
+    print("Resultado:", resultado)
