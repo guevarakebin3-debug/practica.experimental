a0f9ae4be9aee16906680884f46427bc5e41c0ec
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:28:33 -0500

    Update value_error.py

--- Bloque_12/value_error.py
+++ Bloque_12/value_error.py
@@ -1,1 +1,11 @@
+def ejecutar():
 
+    print("Ejercicio 1")
+    print("ValueError")
+
+    try:
+        numero = int(input("Ingrese un número: "))
+        print("Número ingresado:", numero)
+
+    except ValueError:
+        print("Error: Debes ingresar un número válido")
