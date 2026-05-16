ad68319ff8f8ef3f4c219942813f971724e428a7
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:28:47 -0500

    Update index_error.py

--- Bloque_12/index_error.py
+++ Bloque_12/index_error.py
@@ -1,1 +1,21 @@
+def ejecutar():
 
+    print("Bloque 12")
+
+    print("Ejercicio 2")
+    print("IndexError interactivo")
+
+    lista = []
+
+    for i in range(3):
+        num = int(input(f"Ingrese el número {i + 1}: "))
+        lista.append(num)
+
+    print("\nTu lista es:", lista)
+
+    try:
+        pos = int(input("\n¿Qué posición quieres ver? (0-2): "))
+        print("Valor en esa posición:", lista[pos])
+
+    except IndexError:
+        print("Error: esa posición no existe en la lista")
