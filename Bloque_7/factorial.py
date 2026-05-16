0a8046cdbe0e6a753fe967a0c9ee656c6157170e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:52:03 -0500

    Update factorial.py

--- Bloque_7/factorial.py
+++ Bloque_7/factorial.py
@@ -1,32 +1,17 @@
-def doble(x):
-    return x * 2
+def factorial(n):
 
+    if n == 0:
+        return 1
 
-def sumar_varios(*numeros):
-    return sum(numeros)
+    return n * factorial(n - 1)
 
 
 def ejecutar():
 
     print("Bloque 7")
-    print("Ejercicio 1")
-    print("Calcular el doble de un número")
+    print("Ejercicio 3")
+    print("Calcular factorial recursivo")
 
     numero = int(input("Ingrese un número: "))
 
-    print("El doble es:", doble(numero))
-
-    print("\nEjercicio 2")
-    print("Suma de varios números")
-
-    cantidad = int(input("¿Cuántos números desea ingresar?: "))
-
-    lista_numeros = []
-
-    for i in range(cantidad):
-        numero = float(input(f"Ingrese el número {i + 1}: "))
-        lista_numeros.append(numero)
-
-    resultado = sumar_varios(*lista_numeros)
-
-    print("La suma total es:", resultado)
+    print("El factorial es:", factorial(numero))
