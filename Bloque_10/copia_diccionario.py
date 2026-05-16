97ed431e798bc129a5d085c2b63d975b31901002
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:21:32 -0500

    Update copia_diccionario.py

--- Bloque_10/copia_diccionario.py
+++ Bloque_10/copia_diccionario.py
@@ -1,1 +1,36 @@
+def ejecutar():
 
+    print("Bloque 10")
+
+    print("Ejercicio 3")
+    print("Copia de diccionarios")
+
+    datos = {
+        "a": 1,
+        "b": 2
+    }
+
+    copia = datos  # referencia, no copia real
+
+    copia["b"] = 99
+
+    print("Datos original:", datos)
+    print("Copia:", copia)
+
+    print("\nEjercicio adicional")
+    print("Referencia en diccionarios")
+
+    datos = {
+        "a": 10,
+        "b": 20
+    }
+
+    copia = datos
+
+    copia["b"] = 99
+
+    print("\nDatos original:")
+    print(datos)
+
+    print("\nCopia:")
+    print(copia)
