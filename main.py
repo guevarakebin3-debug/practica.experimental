06f798ed36486570083aae542b943a3b7759a8f0
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:07:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from menu import mostrar_menu
 from Bloque_0.persona import ejecutar as persona
-from Bloque_1.producto import ejecutar as product
+from Bloque_1.producto import ejecutar as producto
 
 # FUNCIONES DE EJEMPLO
 # (luego aquí conectas tus archivos reales)
