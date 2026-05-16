26090b8d464fffdfd18b0b73c6c768f0447f5f21
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:15:05 -0500

    Update coordenadas.py

--- Bloque_9/coordenadas.py
+++ Bloque_9/coordenadas.py
@@ -1,1 +1,22 @@
+def ejecutar():
 
+    print("Bloque 9")
+
+    print("Ejercicio 3")
+    print("Lista de coordenadas")
+
+    coordenadas = []
+
+    cantidad = int(input("¿Cuántas coordenadas desea ingresar?: "))
+
+    for i in range(cantidad):
+
+        x = float(input(f"Ingrese x de la coordenada {i + 1}: "))
+        y = float(input(f"Ingrese y de la coordenada {i + 1}: "))
+
+        coordenadas.append((x, y))
+
+    print("\nCoordenadas ingresadas:")
+
+    for x, y in coordenadas:
+        print(f"x = {x}, y = {y}")
