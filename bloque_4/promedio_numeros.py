6570ac721ccf55a9f30b6e8f10e43f31c73b3976
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:31:41 -0500

    Update promedio_numeros.py

--- bloque_4/promedio_numeros.py
+++ bloque_4/promedio_numeros.py
@@ -1,1 +1,14 @@
+def ejecutar():
 
+    print("Bloque 4")
+    print("Ejercicio 2")
+    print("Promedio de dos números")
+
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+
+    suma = num1 + num2
+    promedio = suma / 2
+
+    print("Suma:", suma)
+    print("Promedio:", promedio)
