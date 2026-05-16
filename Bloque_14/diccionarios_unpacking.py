2873e72e5c45a1005d7e441ad3cda0b9094e558f
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:57:16 -0500

    Update diccionarios_unpacking.py

--- Bloque_14/diccionarios_unpacking.py
+++ Bloque_14/diccionarios_unpacking.py
@@ -1,1 +1,21 @@
+def ejecutar():
 
+    print("Bloque 14 - Unpacking de diccionarios")
+
+    print("Ejercicio 3 - Unpacking de diccionarios")
+
+    dict1 = {
+        "nombre": input("Ingrese su nombre: "),
+        "edad": int(input("Ingrese su edad: "))
+    }
+
+    dict2 = {
+        "ciudad": input("Ingrese su ciudad: "),
+        "pais": input("Ingrese su país: ")
+    }
+
+    combinado = {**dict1, **dict2}
+
+    print("\nDiccionario 1:", dict1)
+    print("Diccionario 2:", dict2)
+    print("Combinado:", combinado)
