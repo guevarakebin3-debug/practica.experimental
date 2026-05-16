8f38b7912ed575dd320614e2b17ca332a3342431
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:51:10 -0500

    Update factorial.py

--- Bloque_7/factorial.py
+++ Bloque_7/factorial.py
@@ -1,1 +1,32 @@
+def doble(x):
+    return x * 2
 
+
+def sumar_varios(*numeros):
+    return sum(numeros)
+
+
+def ejecutar():
+
+    print("Bloque 7")
+    print("Ejercicio 1")
+    print("Calcular el doble de un número")
+
+    numero = int(input("Ingrese un número: "))
+
+    print("El doble es:", doble(numero))
+
+    print("\nEjercicio 2")
+    print("Suma de varios números")
+
+    cantidad = int(input("¿Cuántos números desea ingresar?: "))
+
+    lista_numeros = []
+
+    for i in range(cantidad):
+        numero = float(input(f"Ingrese el número {i + 1}: "))
+        lista_numeros.append(numero)
+
+    resultado = sumar_varios(*lista_numeros)
+
+    print("La suma total es:", resultado)
