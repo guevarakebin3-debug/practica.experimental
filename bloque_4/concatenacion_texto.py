023f77c3b1f228de4748bc865619d6606695ec10
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 18:53:07 -0500

    Delete bloque_4 directory

--- bloque_4/concatenacion_texto.py
+++
@@ -1,8 +0,0 @@
-def ejecutar():
-
-    print("Ejercicio 3")
-    print("Concatenación de texto con el número 5")
-
-    numero = input("Ingrese un número: ")
-
-    print(numero + "5")
--- bloque_4/entrada_datos.py
+++
@@ -1,9 +0,0 @@
-def ejecutar():
-
-    print("Ejercicio 1")
-    print("Solicitud de nombre y edad")
-
-    nombre = input("Ingrese su nombre: ")
-    edad = int(input("Ingrese su edad: "))
-
-    print(f"Hola {nombre}, tienes {edad} años.")
