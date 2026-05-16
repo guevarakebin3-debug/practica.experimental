ad1a50e9cf86a9672d4730f80734c683af0ca25a
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:52:19 -0500

    Update operaciones.py

--- Bloque_7/operaciones.py
+++ Bloque_7/operaciones.py
@@ -1,1 +1,21 @@
+def operaciones(numero):
 
+    doble = numero * 2
+    triple = numero * 3
+    cuadrado = numero ** 2
+
+    return doble, triple, cuadrado
+
+
+def ejecutar():
+
+    print("Ejercicio adicional")
+    print("Calcular doble, triple y cuadrado")
+
+    num = float(input("Ingrese un número: "))
+
+    d, t, c = operaciones(num)
+
+    print("Doble:", d)
+    print("Triple:", t)
+    print("Cuadrado:", c)
