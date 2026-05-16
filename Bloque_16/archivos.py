e1d0e6e4e2c3bb4405f0c8d9b1304ace84072411
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:39:51 -0500

    Update archivos.py

--- Bloque_16/archivos.py
+++ Bloque_16/archivos.py
@@ -1,1 +1,15 @@
+def ejecutar():
 
+    print("Bloque 16")
+
+    print("Ejercicio 1 - Archivos")
+
+    # ESCRIBIR
+    with open("archivo.txt", "w") as f:
+        f.write("Python\n")
+
+    # LEER
+    with open("archivo.txt", "r") as f:
+        contenido = f.read()
+        print("Contenido del archivo:")
+        print(contenido)
