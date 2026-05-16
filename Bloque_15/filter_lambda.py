fef9c0b76997b210b1de5e06317f6ecac1609b0f
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:51:02 -0500

    Update filter_lambda.py

--- Bloque_15/filter_lambda.py
+++ Bloque_15/filter_lambda.py
@@ -1,1 +1,16 @@
+def ejecutar():
 
+    print("Bloque 15 - filter + lambda")
+
+    print("Ejercicio 2 - Obtener números mayores a 3")
+
+    numeros = []
+
+    for i in range(3):
+        num = int(input(f"Ingrese número {i + 1}: "))
+        numeros.append(num)
+
+    resultado = list(filter(lambda x: x > 3, numeros))
+
+    print("\nLista original:", numeros)
+    print("Mayores a 3:", resultado)
