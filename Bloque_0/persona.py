cfee1ebd64ed688c8fc5e8d6d7b9acc91eda50a2
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 15:28:42 -0500

    Update and rename person.py to persona.py

---
+++ Bloque_0/persona.py
@@ -0,0 +1,26 @@
+from Bloque_0.person import ejecutar
+ejecutar()
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def mostrar(self):
+        return f"Nombre: {self.nombre} | Edad: {self.edad}"
+
+
+def ejecutar():
+
+    print("Descripción:")
+    print("Crear una clase Persona y tres objetos.\n")
+
+    persona1 = Persona("Carlos", 20)
+    persona2 = Persona("Ana", 25)
+    persona3 = Persona("Luis", 30)
+
+    print("Datos registrados:")
+    print(persona1.mostrar())
+    print(persona2.mostrar())
+    print(persona3.mostrar())
