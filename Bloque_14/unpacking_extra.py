354849a6adfad3e8fafaeda7b2066901d7a3988a
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 17:57:27 -0500

    Update unpacking_extra.py

--- Bloque_14/unpacking_extra.py
+++ Bloque_14/unpacking_extra.py
@@ -1,1 +1,21 @@
+def ejecutar():
 
+    print("Bloque 14 - Unpacking avanzado")
+
+    print("Ejercicio adicional")
+
+    numeros = []
+
+    for i in range(4):
+        num = int(input(f"Ingrese número {i + 1}: "))
+        numeros.append(num)
+
+    primero, *medio, ultimo = numeros
+
+    suma_medio = sum(medio)
+
+    print("\nLista completa:", numeros)
+    print("Primero:", primero)
+    print("Medio:", medio)
+    print("Ultimo:", ultimo)
+    print("Suma del medio:", suma_medio)
