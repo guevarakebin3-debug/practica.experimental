3fa5c5494cf577061d9ac0afed5537f6e6532f6b
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:30:59 -0500

    Update entrada_datos.py

--- bloque_4/entrada_datos.py
+++ bloque_4/entrada_datos.py
@@ -1,1 +1,10 @@
+def ejecutar():
 
+    print("Bloque 4")
+    print("Ejercicio 1")
+    print("Solicitud de nombre y edad")
+
+    nombre = input("Ingrese su nombre: ")
+    edad = int(input("Ingrese su edad: "))
+
+    print(f"Hola {nombre}, tienes {edad} años.")
