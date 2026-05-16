0dc6a8d682dce77af5fc6b98452a2604901fc603
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-16 16:45:18 -0500

    Add function to check if a number is even or odd

--- Bloque_5/par_impar.py
+++ Bloque_5/par_impar.py
@@ -1,1 +1,10 @@
+def ejecutar():
+  
+    print("Verificar si un número es par o impar")
 
+    numero = int(input("Ingrese un número: "))
+
+    if numero % 2 == 0:
+        print("Es par")
+    else:
+        print("Es impar")
