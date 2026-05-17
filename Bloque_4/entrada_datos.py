4bb309346611739c634c3085727d9e7b9803171a
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 08:52:03 -0500

    Update entrada_datos.py

--- Bloque_4/entrada_datos.py
+++ Bloque_4/entrada_datos.py
@@ -1,9 +1,33 @@
 def ejecutar():
-
-    print("Ejercicio 1")
     print("Solicitud de nombre y edad")
 
-    nombre = input("Ingrese su nombre: ")
-    edad = int(input("Ingrese su edad: "))
+    # Validar nombre
+    while True:
+
+        nombre = input("Ingrese su nombre: ").strip()
+
+        if nombre == "":
+            print("❌ El nombre no puede estar vacío")
+        else:
+            break
+
+    # Validar edad
+    while True:
+
+        try:
+
+            edad = int(input("Ingrese su edad: "))
+
+            if edad < 0:
+                print("❌ La edad no puede ser negativa")
+
+            elif edad > 120:
+                print("❌ Edad no válida")
+
+            else:
+                break
+
+        except ValueError:
+            print("❌ Debe ingresar números")
 
     print(f"Hola {nombre}, tienes {edad} años.")
