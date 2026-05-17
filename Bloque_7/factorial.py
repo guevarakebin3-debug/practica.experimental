7f34d67b6cb803f8a80ecdf7b4916038077b86cb
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 09:44:35 -0500

    Update factorial.py

--- Bloque_7/factorial.py
+++ Bloque_7/factorial.py
@@ -1,17 +1,36 @@
-def factorial(n):
-
-    if n == 0:
-        return 1
-
-    return n * factorial(n - 1)
-
-
 def ejecutar():
+
+    def factorial(n):
+
+        if n == 0:
+            return 1
+
+        return n * factorial(n - 1)
 
     print("Bloque 7")
     print("Ejercicio 3")
     print("Calcular factorial recursivo")
 
-    numero = int(input("Ingrese un número: "))
+    # Validar número
+    while True:
+
+        entrada = input("Ingrese un número: ").strip()
+
+        if entrada == "":
+            print("❌ No puede estar vacío")
+            continue
+
+        try:
+
+            numero = int(entrada)
+
+            if numero < 0:
+                print("❌ No se permite números negativos")
+                continue
+
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número entero")
 
     print("El factorial es:", factorial(numero))
