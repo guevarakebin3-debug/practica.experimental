6eeb02d2ecde0198dc759138065087326421f4fb
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 09:05:54 -0500

    Update par_impar.py

--- Bloque_5/par_impar.py
+++ Bloque_5/par_impar.py
@@ -1,8 +1,16 @@
 def ejecutar():
-  
+
     print("Verificar si un número es par o impar")
 
-    numero = int(input("Ingrese un número: "))
+    while True:
+
+        try:
+
+            numero = int(input("Ingrese un número: "))
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número ")
 
     if numero % 2 == 0:
         print("Es par")
