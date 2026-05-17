7135af45db3f89f97ab4333332a3074939e1e147
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 13:18:54 -0500

    Update index_error.py

--- Bloque_12/index_error.py
+++ Bloque_12/index_error.py
@@ -1,18 +1,33 @@
 def ejecutar():
 
-    print("IndexError interactivo")
+    print("IndexError al acceder a numero de la lista")
 
     lista = []
 
+    # Validar números de la lista
     for i in range(3):
-        num = int(input(f"Ingrese el número {i + 1}: "))
-        lista.append(num)
+
+        while True:
+            try:
+                num = int(input(f"Ingrese el número {i + 1}: "))
+                lista.append(num)
+                break
+            except ValueError:
+                print("❌ Debe ingresar un número entero")
 
     print("\nTu lista es:", lista)
 
-    try:
-        pos = int(input("\n¿Qué posición quieres ver? (0-2): "))
-        print("Valor en esa posición:", lista[pos])
+    # Validar posición
+    while True:
 
-    except IndexError:
-        print("Error: esa posición no existe en la lista")
+        try:
+            pos = int(input("\n¿Qué posición quieres ver? (0-2): "))
+
+            print("Valor en esa posición:", lista[pos])
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número entero")
+
+        except IndexError:
+            print("❌ Esa posición no existe en la lista")
