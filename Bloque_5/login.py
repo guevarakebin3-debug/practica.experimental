6f908a65e875474d1afb5b64c3deb1797be28c43
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 09:17:35 -0500

    Update login.py

--- Bloque_5/login.py
+++ Bloque_5/login.py
@@ -3,10 +3,15 @@ def ejecutar():
     print("Sistema de login")
     print("Usuario: admin | password: 123")
 
-    usuario = input("Ingrese el usuario: ")
-    password = input("Ingrese la contraseña: ")
+    while True:
 
-    if usuario == "admin" and password == "123":
-        print("Bienvenido")
-    else:
-        print("Acceso denegado")
+        usuario = input("Ingrese el usuario: ")
+        password = input("Ingrese la contraseña: ")
+
+        if usuario == "admin" and password == "123":
+            print("✅ Bienvenido")
+            break
+
+        else:
+            print("❌ Acceso denegado")
+            print("Intente nuevamente\n")
