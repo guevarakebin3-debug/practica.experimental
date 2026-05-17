5005a14039681a483f75ec7e23f83aa8cc2d7ede
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 08:53:21 -0500

    Update promedio_numeros.py

--- Bloque_4/promedio_numeros.py
+++ Bloque_4/promedio_numeros.py
@@ -1,10 +1,25 @@
 def ejecutar():
-
-    print("Ejercicio 2")
     print("Promedio de dos números")
 
-    num1 = float(input("Ingrese el primer número: "))
-    num2 = float(input("Ingrese el segundo número: "))
+    # Primer número
+    while True:
+
+        try:
+            num1 = float(input("Ingrese el primer número: "))
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número válido")
+
+    # Segundo número
+    while True:
+
+        try:
+            num2 = float(input("Ingrese el segundo número: "))
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número válido")
 
     suma = num1 + num2
     promedio = suma / 2
