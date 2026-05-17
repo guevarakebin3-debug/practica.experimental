fc3bf9caedf810bc507dd8b6606b96662718c7b2
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 13:08:36 -0500

    Update duplicados.py

--- Bloque_11/duplicados.py
+++ Bloque_11/duplicados.py
@@ -5,13 +5,31 @@ def ejecutar():
     print("Ejercicio 2")
     print("Eliminar duplicados con set")
 
-    cantidad = int(input("¿Cuántos números vas a ingresar?: "))
+    # Validar cantidad
+    while True:
+        try:
+            cantidad = int(input("¿Cuántos números vas a ingresar?: "))
+
+            if cantidad < 0:
+                print("❌ No puede ser negativo")
+            else:
+                break
+
+        except ValueError:
+            print("❌ Debe ingresar un número entero")
 
     lista = []
 
     for i in range(cantidad):
-        num = int(input(f"Ingrese el número {i + 1}: "))
-        lista.append(num)
+
+        # Validar cada número
+        while True:
+            try:
+                num = int(input(f"Ingrese el número {i + 1}: "))
+                lista.append(num)
+                break
+            except ValueError:
+                print("❌ Debe ingresar un número entero")
 
     print("\nLista original:", lista)
 
