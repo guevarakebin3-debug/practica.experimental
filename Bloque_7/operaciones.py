cb05d147cc805c4ab401b4476b851ebd82776e58
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 09:40:54 -0500

    Update operaciones.py

--- Bloque_7/operaciones.py
+++ Bloque_7/operaciones.py
@@ -1,18 +1,31 @@
-def operaciones(numero):
-
-    doble = numero * 2
-    triple = numero * 3
-    cuadrado = numero ** 2
-
-    return doble, triple, cuadrado
-
-
 def ejecutar():
+
+    def operaciones(numero):
+
+        doble = numero * 2
+        triple = numero * 3
+        cuadrado = numero ** 2
+
+        return doble, triple, cuadrado
 
     print("Ejercicio adicional")
     print("Calcular doble, triple y cuadrado")
 
-    num = float(input("Ingrese un número: "))
+    # Validar número
+    while True:
+
+        entrada = input("Ingrese un número: ").strip()
+
+        if entrada == "":
+            print("❌ No puede estar vacío")
+            continue
+
+        try:
+            num = float(entrada)
+            break
+
+        except ValueError:
+            print("❌ Debe ingresar un número válido")
 
     d, t, c = operaciones(num)
 
