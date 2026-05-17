bd345d0681a450fb83296d3295180d07513a00de
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 13:20:35 -0500

    Update division_error.py

--- Bloque_12/division_error.py
+++ Bloque_12/division_error.py
@@ -3,15 +3,18 @@ def ejecutar():
     print("Ejercicio 3")
     print("División con manejo de errores")
 
-    try:
-        a = int(input("Ingrese el primer número: "))
-        b = int(input("Ingrese el segundo número: "))
+    while True:
 
-        resultado = a / b
-        print("Resultado:", resultado)
+        try:
+            a = int(input("Ingrese el primer número: "))
+            b = int(input("Ingrese el segundo número: "))
 
-    except ValueError:
-        print("Error: debes ingresar solo números")
+            resultado = a / b
+            print("Resultado:", resultado)
+            break  # sale si todo está bien
 
-    except ZeroDivisionError:
-        print("Error: no se puede dividir entre cero")
+        except ValueError:
+            print("❌ Error: debes ingresar solo números")
+
+        except ZeroDivisionError:
+            print("❌ Error: no se puede dividir entre cero")
