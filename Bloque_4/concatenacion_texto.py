e2e5092774e244256dc852c51ad653d3fec4ad2e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 08:54:48 -0500

    Update concatenacion_texto.py

--- Bloque_4/concatenacion_texto.py
+++ Bloque_4/concatenacion_texto.py
@@ -1,8 +1,14 @@
 def ejecutar():
-
-    print("Ejercicio 3")
     print("Concatenación de texto con el número 5")
 
-    numero = input("Ingrese un número: ")
+    while True:
+
+        numero = input("Ingrese un número: ")
+
+        if numero.isdigit():
+            break
+
+        else:
+            print("❌ Debe ingresar solo números")
 
     print(numero + "5")
