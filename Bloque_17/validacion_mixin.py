8d17c28808984d3b0253d122e7a5c13c0bb12b3e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 21:27:24 -0500

    Update validacion_mixin.py

--- Bloque_17/validacion_mixin.py
+++ Bloque_17/validacion_mixin.py
@@ -1,6 +1,6 @@
 class ValidacionMixin:
     def validar_email(self, correo):
-        return "@" in correo and ".com" in correo
+        return "@" in correo and correo.endswith(".com")
 
     def validar_edad(self, edad):
         return edad >= 18
@@ -9,12 +9,14 @@ class ValidacionMixin:
 class Usuario(ValidacionMixin):
     def registrar(self, nombre, edad, correo):
 
-        if not self.validar_email(correo):
-            print("❌ Correo inválido")
-            return
-
         if not self.validar_edad(edad):
             print("❌ Edad inválida (debe ser mayor o igual a 18)")
+            print("🔁 Intenta registrarte nuevamente\n")
+            return
+
+        if not self.validar_email(correo):
+            print("❌ Correo inválido")
+            print("🔁 Intenta registrarte nuevamente\n")
             return
 
         print("\n✅ Usuario registrado correctamente")
@@ -25,11 +27,28 @@ class Usuario(ValidacionMixin):
 
 def ejecutar():
 
-    print("Bloque 17 - ValidacionMixin")
-
-    nombre = input("Ingrese nombre: ")
-    edad = int(input("Ingrese edad: "))
-    correo = input("Ingrese correo: ")
-
     usuario = Usuario()
-    usuario.registrar(nombre, edad, correo)
+
+    while True:
+
+        print("\n--- Registro ---")
+
+        nombre = input("Nombre: ")
+        edad = int(input("Edad: "))
+
+        if not usuario.validar_edad(edad):
+            print(" Edad inválida")
+            print("Debes registrarte nuevamente\n")
+            continue
+
+        correo = input("Correo: ")
+
+        if not usuario.validar_email(correo):
+            print(" Correo inválido")
+            print(" Debes registrarte nuevamente\n")
+            continue
+
+        usuario.registrar(nombre, edad, correo)
+
+        print("🎉 Proceso finalizado correctamente")
+        break
