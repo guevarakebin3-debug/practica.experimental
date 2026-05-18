eb1aa1af091d7c37d04af0dde954981dd55effce
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-17 20:53:43 -0500

    Update archivos.py

--- Bloque_16/archivos.py
+++ Bloque_16/archivos.py
@@ -1,15 +1,22 @@
 def ejecutar():
 
     print("Bloque 16")
-
     print("Ejercicio 1 - Archivos")
+
+    while True:
+        texto = input("Escribe 'Python': ")
+
+        if texto == "Python":
+            break
+        else:
+            print("Error: debes escribir exactamente 'Python'.")
 
     # ESCRIBIR
     with open("archivo.txt", "w") as f:
-        f.write("Python\n")
+        f.write(texto + "\n")
 
     # LEER
     with open("archivo.txt", "r") as f:
-        contenido = f.read()
-        print("Contenido del archivo:")
-        print(contenido)
+        print("\nContenido del archivo:")
+        print(f.read())
+
