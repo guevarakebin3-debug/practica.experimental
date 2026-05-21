a9e5049a08b22aab8714b3273b89908b2a6ea814
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-21 03:27:07 -0500

    Update ciclos.py

--- Bloque_6/ciclos.py
+++ Bloque_6/ciclos.py
@@ -1,30 +1,22 @@
-def ejecutar():
-    print("\nEjercicio 1")
+def run():
+
     print("Imprime números del 1 al 10")
 
-    contador = 1
+    counter = 1
 
-    while contador <= 10:
-        print(contador)
-        contador += 1
+    while counter <= 10:
+        print(counter)
+        counter += 1
 
+    print("\nEnumerate con frutas")
 
-    
-    print("\nEjercicio 2")
-    print("Enumerate con frutas")
+    fruits = ["manzana", "pera", "uva"]
 
-    frutas = ["manzana", "pera", "uva"]
+    for index, fruit in enumerate(fruits):
+        print(index, fruit)
 
-    for indice, fruta in enumerate(frutas):
-        print(indice, fruta)
+    print("\nCuadrados pares entre 1 y 10")
 
+    squares = [x**2 for x in range(1, 11) if x % 2 == 0]
 
-
-    
-
-    print("\nEjercicio 3")
-    print("Cuadrados pares entre 1 y 10")
-
-    cuadrados = [x**2 for x in range(1, 11) if x % 2 == 0]
-
-    print(cuadrados)
+    print(squares)
