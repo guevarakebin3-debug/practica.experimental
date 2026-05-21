ec0d3329d2edafffcffe16c90f415c12964dd0db
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-21 03:45:26 -0500

    Update conjuntos.py

--- Bloque_11/conjuntos.py
+++ Bloque_11/conjuntos.py
@@ -1,57 +1,57 @@
-def ejecutar():
-    print("Ejercicio 1")
+def run():
+
     print("Conjuntos interactivos")
 
-    A = set()
-    B = set()
+    set_a = set()
+    set_b = set()
 
     # Validar cantidad A
     while True:
         try:
-            cantidad_a = int(input("¿Cuántos números tendrá el conjunto A?: "))
-            if cantidad_a < 0:
-                print("❌ No puede ser negativo")
+            size_a = int(input("¿Cuántos números tendrá el conjunto A?: "))
+            if size_a < 0:
+                print("No puede ser negativo")
             else:
                 break
         except ValueError:
-            print("❌ Debe ingresar un número entero")
+            print("Debe ingresar un número entero")
 
     # Llenar A
-    for i in range(cantidad_a):
+    for i in range(size_a):
 
         while True:
             try:
                 num = int(input(f"Ingrese número {i + 1} para A: "))
-                A.add(num)
+                set_a.add(num)
                 break
             except ValueError:
-                print("❌ Debe ingresar un número entero")
+                print("Debe ingresar un número entero")
 
     # Validar cantidad B
     while True:
         try:
-            cantidad_b = int(input("\n¿Cuántos números tendrá el conjunto B?: "))
-            if cantidad_b < 0:
-                print("❌ No puede ser negativo")
+            size_b = int(input("\n¿Cuántos números tendrá el conjunto B?: "))
+            if size_b < 0:
+                print("No puede ser negativo")
             else:
                 break
         except ValueError:
-            print("❌ Debe ingresar un número entero")
+            print("Debe ingresar un número entero")
 
     # Llenar B
-    for i in range(cantidad_b):
+    for i in range(size_b):
 
         while True:
             try:
                 num = int(input(f"Ingrese número {i + 1} para B: "))
-                B.add(num)
+                set_b.add(num)
                 break
             except ValueError:
-                print("❌ Debe ingresar un número entero")
+                print("Debe ingresar un número entero")
 
-    print("\nConjunto A:", A)
-    print("Conjunto B:", B)
+    print("\nConjunto A:", set_a)
+    print("Conjunto B:", set_b)
 
-    print("\nUnión (A | B):", A | B)
-    print("Intersección (A & B):", A & B)
-    print("Diferencia (A - B):", A - B)
+    print("\nUnión (A | B):", set_a | set_b)
+    print("Intersección (A & B):", set_a & set_b)
+    print("Diferencia (A - B):", set_a - set_b)
