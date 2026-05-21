9ce06e3c01817e9ee44852a480ee20479556d9aa
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-21 03:59:22 -0500

    Update reduce_lambda.py

--- Bloque_15/reduce_lambda.py
+++ Bloque_15/reduce_lambda.py
@@ -1,32 +1,32 @@
 from functools import reduce
 
-def ejecutar():
+def run():
 
-    print(" Multiplica todos los elementos con reduce")
+    print("Multiplica todos los elementos con reduce")
 
-    numeros = [1, 2, 3, 4]
+    numbers = [1, 2, 3, 4]
 
-    resultado = reduce(lambda x, y: x * y, numeros)
+    result = reduce(lambda x, y: x * y, numbers)
 
-    print("Lista:", numeros)
-    print("Resultado de la multiplicación:", resultado)
+    print("Lista:", numbers)
+    print("Resultado de la multiplicación:", result)
 
 
-    print("\nEjercicio adicional_Multiplicación con reduce")
+    print("\nEjercicio adicional: Multiplicación con reduce")
 
-    numeros = []
+    numbers = []
 
     for i in range(3):
         while True:
             try:
                 num = int(input(f"Ingrese número {i + 1}: "))
-                numeros.append(num)
+                numbers.append(num)
                 break
             except ValueError:
-                print("Error: debe ingresar un número entero válido.")
+                print("❌ Error: debe ingresar un número entero válido.")
 
-    print("\nLista ingresada:", numeros)
+    print("\nLista ingresada:", numbers)
 
-    resultado = reduce(lambda x, y: x * y, numeros)
+    result = reduce(lambda x, y: x * y, numbers)
 
-    print("Resultado:", resultado)
+    print("Resultado:", result)
