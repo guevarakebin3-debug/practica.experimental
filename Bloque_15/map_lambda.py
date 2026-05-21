e31b918eb983c5c07b42acca768b0cc0e95699ad
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-21 03:58:29 -0500

    Update map_lambda.py

--- Bloque_15/map_lambda.py
+++ Bloque_15/map_lambda.py
@@ -1,10 +1,10 @@
-def ejecutar():
+def run():
     
     print("Incrementa en 1 cada elemento de la lista usando map")
 
-    numeros = [2, 4, 6]
+    numbers = [2, 4, 6]
 
-    resultado = list(map(lambda x: x + 1, numeros))
+    result = list(map(lambda x: x + 1, numbers))
 
-    print("Lista original:", numeros)
-    print("Resultado:", resultado)
+    print("Lista original:", numbers)
+    print("Resultado:", result)
