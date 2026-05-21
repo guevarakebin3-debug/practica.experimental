c83c90a7649517c6a8d02d4835584083fde1279e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-21 04:05:24 -0500

    Update promedio_mixin.py

--- Bloque_17/promedio_mixin.py
+++ Bloque_17/promedio_mixin.py
@@ -1,48 +1,48 @@
-class PromedioMixin:
-    def calcular_promedio(self, notas):
-        return sum(notas) / len(notas)
+class AverageMixin:
+    def calculate_average(self, grades):
+        return sum(grades) / len(grades)
 
 
-class Estudiante(PromedioMixin):
-    def __init__(self, nombre):
-        self.nombre = nombre
+class Student(AverageMixin):
+    def __init__(self, name):
+        self.name = name
 
-    def mostrar_promedio(self, notas):
-        promedio = self.calcular_promedio(notas)
-        print(f"\nEstudiante: {self.nombre}")
-        print("Promedio:", promedio)
+    def show_average(self, grades):
+        average = self.calculate_average(grades)
+        print(f"\nEstudiante: {self.name}")
+        print("Promedio:", average)
 
 
-def ejecutar():
+def run():
 
-    print("Bloque 17 - PromedioMixin")
+    print("PromedioMixin")
 
     # VALIDACIÓN DEL NOMBRE
     while True:
-        nombre = input("Ingrese nombre del estudiante: ")
+        name = input("Ingrese nombre del estudiante: ")
 
-        if nombre.strip() == "":
-            print("Error: el nombre no puede estar vacío.")
-        elif nombre.isdigit():
-            print("Error: el nombre no puede ser solo números.")
+        if name.strip() == "":
+            print("❌ Error: el nombre no puede estar vacío.")
+        elif name.isdigit():
+            print("❌ Error: el nombre no puede ser solo números.")
         else:
             break
 
     # VALIDACIÓN DE NOTAS
-    notas = []
+    grades = []
 
     for i in range(3):
         while True:
             try:
-                nota = float(input(f"Ingrese nota {i + 1} (0-10): "))
+                grade = float(input(f"Ingrese nota {i + 1} (0-10): "))
 
-                if 0 <= nota <= 10:
-                    notas.append(nota)
+                if 0 <= grade <= 10:
+                    grades.append(grade)
                     break
                 else:
-                    print("Error: la nota debe estar entre 0 y 10.")
+                    print("❌ Error: la nota debe estar entre 0 y 10.")
             except ValueError:
-                print("Error: debe ingresar un número válido.")
+                print("❌ Error: debe ingresar un número válido.")
 
-    est = Estudiante(nombre)
-    est.mostrar_promedio(notas)
+    student = Student(name)
+    student.show_average(grades)
