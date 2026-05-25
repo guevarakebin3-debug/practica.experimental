8c4a5f5ba433960a456bd7264276636995fef867
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:00:40 -0500

    Update estudiante.py

--- Bloque_1/estudiante.py
+++ Bloque_1/estudiante.py
@@ -6,38 +6,19 @@ class Student:
     @classmethod
     def from_dictionary(cls, data):
         return cls(
-            data.get("nombre", ""),
+            data.get("name", ""),
             data.get("grades", [])
         )
 
 
-def ask_student():
-    name = input("Nombre: ").strip()
-
-    entry = input("Notas (separadas por espacio): ").strip()
-    grades = list(map(float, entry.split())) if entry else []
-
-    return name, grades
-
-
 def run():
-    print("Crear estudiante")
-    name, grades = ask_student()
-    student = Student(name, grades)
+    # Un solo estudiante con datos fijos
+    data = {
+        "name": "Carlos",
+        "grades": [9.5, 8.7, 10]
+    }
+
+    student = Student.from_dictionary(data)
 
     print("Resultado:")
     print(student.name, student.grades)
-
-    print("\nCrear estudiante desde diccionario")
-
-    name, grades = ask_student()
-
-    data = {
-        "name": name,
-        "grades": grades
-    }
-
-    student2 = Student.from_dictionary(data)
-
-    print("Resultado:")
-    print(student2.name, student2.grades)
