f55873014a8f70224e66a2a1cd3b7919fb18c62d
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:09:24 -0500

    Update coordenadas.py

--- Bloque_9/coordenadas.py
+++ Bloque_9/coordenadas.py
@@ -2,44 +2,17 @@ def run():
 
     print("Lista de coordenadas")
 
-    coordinates = []
-
-    # Validar cantidad de coordenadas
-    while True:
-
-        try:
-            quantity = int(input("¿Cuántas coordenadas desea ingresar?: "))
-
-            if quantity <= 0:
-                print("❌ Debe ser mayor a 0")
-                continue
-
-            break
-
-        except ValueError:
-            print("❌ Debe ingresar un número entero")
-
-    for i in range(quantity):
-
-        # Validar X
-        while True:
-            try:
-                x = float(input(f"Ingrese x de la coordenada {i + 1}: "))
-                break
-            except ValueError:
-                print("❌ Debe ingresar un número válido")
-
-        # Validar Y
-        while True:
-            try:
-                y = float(input(f"Ingrese y de la coordenada {i + 1}: "))
-                break
-            except ValueError:
-                print("❌ Debe ingresar un número válido")
-
-        coordinates.append((x, y))
+    # Datos fijos
+    coordinates = [
+        (2.5, 4.0),
+        (1.2, 3.8),
+        (7.0, 9.1)
+    ]
 
     print("\nCoordenadas ingresadas:")
 
     for x, y in coordinates:
         print(f"x = {x}, y = {y}")
+
+
+
