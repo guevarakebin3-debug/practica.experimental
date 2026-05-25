c0afa0ab13ee7d87f83efaeda118cd3d49943e7c
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:07:04 -0500

    Update funciones.py

--- Bloque_7/funciones.py
+++ Bloque_7/funciones.py
@@ -8,69 +8,15 @@ def run():
 
     print("Calcular el doble de un número")
 
-    # Validar número
-    while True:
-
-        entry = input("Ingrese un número: ").strip()
-
-        if entry == "":
-            print("❌ No puede estar vacío")
-            continue
-
-        try:
-            number = int(entry)
-            break
-
-        except ValueError:
-            print("❌ Debe ingresar un número entero")
+    # Dato fijo
+    number = 8
 
     print("El doble es:", double(number))
 
     print("\nSuma de varios números")
 
-    # Validar cantidad
-    while True:
-
-        entry = input("¿Cuántos números desea ingresar?: ").strip()
-
-        if entry == "":
-            print("❌ No puede estar vacío")
-            continue
-
-        try:
-
-            quantity = int(entry)
-
-            if quantity <= 0:
-                print("❌ Debe ingresar una cantidad mayor a 0")
-                continue
-
-            break
-
-        except ValueError:
-            print("❌ Debe ingresar números enteros")
-
-    numbers_list = []
-
-    # Validar números de la lista
-    for i in range(quantity):
-
-        while True:
-
-            entry = input(f"Ingrese el número {i + 1}: ").strip()
-
-            if entry == "":
-                print("❌  No puede estar vacío")
-                continue
-
-            try:
-
-                number = float(entry)
-                numbers_list.append(number)
-                break
-
-            except ValueError:
-                print("❌  Debe ingresar un número válido")
+    # Datos fijos
+    numbers_list = [10, 20, 30, 40]
 
     result = sum_multiple(*numbers_list)
 
