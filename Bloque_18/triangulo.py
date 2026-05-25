a98099ae05892c4e8ce8097dbd57a78f79686077
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 10:19:57 -0500

    Create triangulo.py

---
+++ Bloque_18/triangulo.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+
+class Triangle(Shape):
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height
+
+    def area(self):
+        return (self.base * self.height) / 2
+
+
+t = Triangle(10, 5)
+print("El área del triángulo es:", t.area())  # salida en español
