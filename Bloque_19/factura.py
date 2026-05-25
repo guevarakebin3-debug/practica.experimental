548f19dd69f2e76e740e9218af3f5f04aeeca94c
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 12:28:53 -0500

    Update factura.py

--- Bloque_19/factura.py
+++ Bloque_19/factura.py
@@ -1,11 +1,10 @@
-# PRODUCT CLASS
 class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
 
 
-# COMPOSITION CLASS
+# COMPOSICION
 class InvoiceDetail:
     def __init__(self, product, quantity):
         self.product = product
@@ -15,7 +14,7 @@ class InvoiceDetail:
         return self.product.price * self.quantity
 
 
-# MAIN CLASS (COMPOSITION)
+# MAIN CLASS
 class Invoice:
     def __init__(self):
         self.details = []
@@ -28,6 +27,10 @@ class Invoice:
 
 
 def run():
+
+    print("\n FACTURA CON COMPOSICIÓN ")
+    print(" Crear una factura que almacene productos, cantidades y calcule el total.\n")
+
     p1 = Product("Teclado", 30)
     p2 = Product("Monitor", 200)
 
@@ -35,5 +38,9 @@ def run():
     invoice.add_product(p1, 2)
     invoice.add_product(p2, 1)
 
-    print("Total de la factura:", invoice.total())
+    print("DETALLE DE FACTURA:")
+    print("- Teclado x2 = 60")
+    print("- Monitor x1 = 200")
+
+    print("\nTOTAL DE LA FACTURA:", invoice.total())
 
