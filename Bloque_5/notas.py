1d1accc95f1b1b6d1b188ea2d7a48d280fdaf98d
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:04:54 -0500

    Update notas.py

--- Bloque_5/notas.py
+++ Bloque_5/notas.py
@@ -2,28 +2,8 @@ def run():
 
     print("Nivel de rendimiento académico")
 
-    while True:
-
-        entry = input("Ingrese su nota: ").strip()
-
-        # Vacío
-        if entry == "":
-            print("❌ No puede estar vacío")
-            continue
-
-        try:
-
-            grade = int(entry)
-
-            # Rango válido
-            if grade < 0 or grade > 100:
-                print("❌ La nota debe estar entre 0 y 100")
-                continue
-
-            break
-
-        except ValueError:
-            print("❌ Debe ingresar números enteros")
+    # Dato fijo
+    grade = 85
 
     if grade >= 90:
         print("A")
@@ -36,3 +16,4 @@ def run():
 
     else:
         print("D")
+
