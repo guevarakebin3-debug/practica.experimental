a5f657ff88b92d037cc8ff1adbf66757b0efbc88
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:05:59 -0500

    Update factorial.py

--- Bloque_7/factorial.py
+++ Bloque_7/factorial.py
@@ -9,26 +9,7 @@ def run():
 
     print("Calcular factorial recursivo")
 
-    # Validar número
-    while True:
-
-        entry = input("Ingrese un número: ").strip()
-
-        if entry == "":
-            print("❌ No puede estar vacío")
-            continue
-
-        try:
-
-            number = int(entry)
-
-            if number < 0:
-                print("❌ No se permite números negativos")
-                continue
-
-            break
-
-        except ValueError:
-            print("❌ Debe ingresar un número entero")
+    # Dato fijo
+    number = 10
 
     print("El factorial es:", factorial(number))
