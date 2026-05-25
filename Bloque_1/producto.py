e61365a2dea84f762a10c88cc39e117134fb3f0e
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:01:01 -0500

    Update producto.py

--- Bloque_1/producto.py
+++ Bloque_1/producto.py
@@ -9,36 +9,19 @@ class Product:
         self.price = price
 
 
-def ask_price(product_name):
-    while True:
-        try:
-            price = float(input(f"Ingrese el precio de{product_name}: "))
-            if price < 0:
-                raise ValueError("El precio no puede ser negativo")
-            return price
-        except ValueError as e:
-            print("❌ Error:", e)
-
-
 def run():
     print("=== REGISTRO DE PRODUCTOS FIJOS ===\n")
 
-    # Product 1
-    code1, name1 = "P001", "Laptop"
-    print(f"Product: {code1} - {name1}")
+    try:
+        # Producto 1
+        product1 = Product("P001", "Laptop", 1200)
 
-    price1 = ask_price(name1)
-    product1 = Product(code1, name1, price1)
+        # Producto 2
+        product2 = Product("P002", "Mouse", 30)
 
-    print()
+        print("=== PRODUCTOS REGISTRADOS ===")
+        print(product1.code, product1.name, product1.price)
+        print(product2.code, product2.name, product2.price)
 
-    # Product 2
-    code2, name2 = "P002", "Mouse"
-    print(f"Product: {code2} - {name2}")
-
-    price2 = ask_price(name2)
-    product2 = Product(code2, name2, price2)
-
-    print("\n=== REGISTERED PRODUCTS ===")
-    print(product1.code, product1.name, product1.price)
-    print(product2.code, product2.name, product2.price)
+    except ValueError as e:
+        print("❌ Error:", e)
