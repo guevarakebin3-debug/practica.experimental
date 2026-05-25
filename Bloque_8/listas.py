f713da90a4255ad9ab080d4d3eb5cc502401730f
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 11:08:58 -0500

    Update listas.py

--- Bloque_8/listas.py
+++ Bloque_8/listas.py
@@ -2,26 +2,8 @@ def run():
 
     print("Lista ordenada con 3 elementos")
 
-    numbers_list = []
-
-    for i in range(3):
-
-        # Validar número
-        while True:
-
-            entry = input(f"Ingrese el número {i + 1}: ").strip()
-
-            if entry == "":
-                print("❌ No puede estar vacío")
-                continue
-
-            try:
-                number = int(entry)
-                numbers_list.append(number)
-                break
-
-            except ValueError:
-                print("❌ Debe ingresar un número entero")
+    # Datos fijos
+    numbers_list = [12, 5, 20]
 
     print("Lista original:", numbers_list)
 
@@ -38,3 +20,5 @@ def run():
     print("Suma:", sum(nums))
     print("Máximo:", max(nums))
     print("Mínimo:", min(nums))
+
+
