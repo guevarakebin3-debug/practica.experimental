59155bf39efb784f0ab9ec6f43e012737f7c90fc
guevarakebin3-debug <guevarakebin3@gmail.com>
2026-05-25 10:47:15 -0500

    Update empleado.py

--- Bloque_19/empleado.py
+++ Bloque_19/empleado.py
@@ -1,1 +1,19 @@
 
+class Person:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+
+class Employee(Person):
+    def __init__(self, id, name, salary):
+        super().__init__(id, name)
+        self.salary = salary
+
+
+def run():
+    employee = Employee("E01", "Carlos", 500)
+
+    print("Empleado:", employee.name)
+    print("Salario:", employee.salary)
+
